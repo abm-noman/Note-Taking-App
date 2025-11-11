820dbae9905656bff950cbf6a016345959668feb
abm-noman <noman15-2839@diu.edu.bd>
2025-11-11 16:55:14 +0600

    first commit

---
+++ note-taking-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return ( 
+    <div className="App">
+      <h1>Note Taking App</h1>
+      <p>This is a simple note-taking application.</p>
+      
+    </div>
+
+  )
+}
+
+export default App
