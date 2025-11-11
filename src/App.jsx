bd14c89c619f0ddb3db6ba8460226ca3017936c9
abm-noman <noman15-2839@diu.edu.bd>
2025-11-11 17:16:04 +0600

    Input Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [noteTitle, setNoteTitle] = useState("Note Title...");
+  const changeNoteTitle = (event) => {
+    setNoteTitle(event.target.value);
+  };
 
   return ( 
     <div className="App">
       <h1>Note Taking App</h1>
       <p>This is a simple note-taking application.</p>
-      
+      <form>
+        <input value={noteTitle} type="text" onChange={changeNoteTitle} />
+        <br />
+        <button type="submit">Add a Note</button>
+      </form>
+
     </div>
 
   )
