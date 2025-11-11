b752afd62d5da0504d2da3d9cfd286a8728790fa
abm-noman <noman15-2839@diu.edu.bd>
2025-11-11 21:08:36 +0600

    Completed Edit Note

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,53 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //states
   const [noteTitle, setNoteTitle] = useState("");
   const [notes, setNotes] = useState([]);
   const changeNoteTitle = (event) => {
     setNoteTitle(event.target.value);
   };
+  const [editMode, setEditMode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
+
+  //event handlers
+  //const createHandler = () => {};
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (noteTitle.trim() === "") return;
-    const newNote = {
-      id: Date.now + "",
-      title: noteTitle,
-    };
-    setNotes([...notes, newNote]);
-    setNoteTitle("");
+    if (noteTitle === "") return;
+    if (editMode) {
+      updateHandler();
+    } else {
+      const newNote = {
+        id: Date.now() + "",
+        title: noteTitle,
+      };
+      setNotes([...notes, newNote]);
+      setNoteTitle("");
+    }
   };
 
-  const removeHandler = (NoteId) => {
-    const filteredNotes = notes.filter((note) => note.id !== NoteId);
+  const removeHandler = (noteId) => {
+    const filteredNotes = notes.filter((note) => note.id !== noteId);
     setNotes(filteredNotes);
+  };
+
+  const editHandler = (note) => {
+    setEditMode(true);
+    setEditableNote(note);
+    setNoteTitle(note.title);
+  };
+
+  const updateHandler = () => {
+    const updatedNotes = notes.map((note) =>
+      note.id === editableNote.id ? { ...note, title: noteTitle } : note
+    );
+    setNotes(updatedNotes);
+    setEditMode(false);
+    setEditableNote(null);
+    setNoteTitle("");
   };
 
   return ( 
@@ -31,13 +58,13 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input value={noteTitle} type="text" onChange={changeNoteTitle} style={{ width: "200px", height: "50px", fontSize: "18px", padding: "5px" }}/>
         <br />
-        <button type="submit">Add a Note</button>
+        <button type="submit">{editMode ? "Update Note" : "Add a Note"}</button>
       </form>
       <ul>
         {notes.map((note) => (
           <li key={note.id}>
             <span>{note.title}</span>
-            <button>Edit</button>
+            <button onClick={() => editHandler(note)}>Edit</button>
             <button onClick={() => removeHandler(note.id)}>Remove</button>
           </li>
         ))}
