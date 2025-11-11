8a8727a390a138c3d29c78f4ca279bd99e3276d4
abm-noman <noman15-2839@diu.edu.bd>
2025-11-11 18:26:38 +0600

    Create new note and remove existing note

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,46 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [noteTitle, setNoteTitle] = useState("Note Title...");
+  const [noteTitle, setNoteTitle] = useState("");
+  const [notes, setNotes] = useState([]);
   const changeNoteTitle = (event) => {
     setNoteTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (noteTitle.trim() === "") return;
+    const newNote = {
+      id: Date.now + "",
+      title: noteTitle,
+    };
+    setNotes([...notes, newNote]);
+    setNoteTitle("");
+  };
+
+  const removeHandler = (NoteId) => {
+    const filteredNotes = notes.filter((note) => note.id !== NoteId);
+    setNotes(filteredNotes);
   };
 
   return ( 
     <div className="App">
       <h1>Note Taking App</h1>
       <p>This is a simple note-taking application.</p>
-      <form>
-        <input value={noteTitle} type="text" onChange={changeNoteTitle} />
+      <form onSubmit={handleSubmit}>
+        <input value={noteTitle} type="text" onChange={changeNoteTitle} style={{ width: "200px", height: "50px", fontSize: "18px", padding: "5px" }}/>
         <br />
         <button type="submit">Add a Note</button>
       </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <span>{note.title}</span>
+            <button>Edit</button>
+            <button onClick={() => removeHandler(note.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
 
     </div>
 
